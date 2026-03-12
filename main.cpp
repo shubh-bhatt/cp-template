1cb652bc6aff710073d6de2b680b74de5f8d078d
Shubham Bhatt <159576279+shubh-bhatt@users.noreply.github.com>
2026-03-13 02:18:37 +0530

    Refactor main.cpp to include clean code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
+/*
+    In God I Trust
+    Author: Shubham Bhatt
+*/
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
 #define endl '\n'
 #define int long long
-#define SPEED ios::sync_with_stdio(false); cin.tie(NULL);
 
 const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX >> 1;
 
-int32_t main() {
-	SPEED
-	
-	
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 }
